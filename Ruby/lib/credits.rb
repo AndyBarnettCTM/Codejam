69ee245473c9bde3548066b3c9a64a5d3400c7ee
Andy Barnett <andrew.barnett@comparethemarket.com>
2016-03-15 15:41:18 +0000

    storing opening credits

--- Ruby/lib/credits.rb
+++ Ruby/lib/credits.rb
@@ -1,9 +1,19 @@
 require 'httparty'
+require 'json'
 class Credits
 
-  def self.call
-    HTTParty.get("http://swapi.co/api/films/1/")
+  attr_reader :crawl
 
+  def initialize(film_data)
+    @crawl = film_data.fetch("opening_crawl", "")
+  end
+
+  def self.call
+    request = HTTParty.get("http://swapi.co/api/films/1/")
+    data = JSON.parse(request.body)
+
+
+    self.new(data)
   end
 
 end
--- Ruby/spec/credits_spec.rb
+++ Ruby/spec/credits_spec.rb
@@ -2,8 +2,16 @@ require 'credits'
 
 describe Credits do
 
+  let(:data){File.read('./spec/data.json')}
+  let(:httparty_response){double :response, {body: data}}
   it "gets the correct data from api" do
-    expect(HTTParty).to receive(:get).with("http://swapi.co/api/films/1/")
+    expect(HTTParty).to receive(:get).with("http://swapi.co/api/films/1/").and_return(httparty_response)
     Credits.call
+  end
+
+  it "can store the opening credits" do
+    credits = Credits.call
+    allow(HTTParty).to receive(:get).with("http://swapi.co/api/films/1/").and_return(httparty_response)
+    expect(credits.crawl).to eq "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy...."
   end
 end
